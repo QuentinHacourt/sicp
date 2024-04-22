eefd3e52c2e54b151019d77b831c163de7eb50a6
QuentinHacourt <quentin.hacourt@gmail.com>
2024-04-22 11:53:10 +0200

    found a solution for cut all by myself :)

--- structuur/10_stromen/10.12.1_cut.scm
+++ structuur/10_stromen/10.12.1_cut.scm
@@ -1,12 +1,29 @@
 (load "streams.rkt")
 
-(define (cut stream)
-  (define (split res rem el)
-    (cond ((empty-stream? rem) (cons res the-empty-stream))
-          ((eq? (head rem) el) (split (cons-stream el res) (tail rem) el))
-          (else (cons res rem))))
+(define (print-m matrix)
+	(display "[")
+	(print-stream (head matrix)) (newline)
+	(stream-for-each (lambda (x) (display " ")
+						(print-stream x) (newline))
+					 (tail matrix))
+	(display "]\n"))
 
+(define (append-elem-to-last-stream elem stream)
+  (cond ((empty-stream? stream)
+         (cons-stream (cons-stream elem the-empty-stream) the-empty-stream))
+        ((empty-stream? (tail stream))
+         (cons-stream (cons-stream elem (head stream)) the-empty-stream))
+        (else
+         (cons-stream (head stream) (append-elem-to-last-stream elem (tail stream))))))
+
+(define (add-new-stream-with-elem elem stream)
   (if (empty-stream? stream)
-      the-empty-stream
-      (let ((gesplits ( split the-empty-stream stream (head stream))))
-        (cons-stream (car gesplits) (cut (cdr gesplits))))))  )
+         (cons-stream (cons-stream elem the-empty-stream) the-empty-stream)
+         (cons-stream (head stream) (add-new-stream-with-elem elem (tail stream)))))
+
+(define (cut stream)
+  (define (iter str last res)
+    (cond ((empty-stream? str) res)
+          ((eq? last (head str)) (iter (tail str) last (append-elem-to-last-stream last res)))
+          (else (iter (tail str) (head str) (add-new-stream-with-elem (head str) res)))))
+  (iter stream (head stream) the-empty-stream))
